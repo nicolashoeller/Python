974850c18aecb6a759321a3dbe14b092cede0726
nicolashoeller <hoellr3.0@gmail.com>
2024-02-20 09:14:11 +0100

    Add Python string manipulation examples

---
+++ python-school/aufgabe2.py
@@ -0,0 +1,11 @@
+string = "Python"
+string2 = "Kurs"
+leerzeichen = ' '
+bindestrich = '-'
+
+print(string + leerzeichen + bindestrich + leerzeichen + string2)
+
+print((string + leerzeichen)*3)
+
+print(string[:3])
+print(string[3:])
