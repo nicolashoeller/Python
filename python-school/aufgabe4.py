6fb890a5087dccd7fbfdfbb4e4ddd3b065950103
nicolashoeller <hoellr3.0@gmail.com>
2024-02-20 09:49:30 +0100

    Add code to determine if the store is open or closed based on the day and if it's a holiday

---
+++ python-school/aufgabe4.py
@@ -0,0 +1,22 @@
+sonntag = False  # Setze auf True, wenn heute Sonntag ist
+feiertag = False  # Setze auf True, wenn heute ein Feiertag ist
+
+print("Welcher Tag ist heute?")
+tag = input()
+tag = tag.lower()
+
+if tag == "sonntag":
+    sonntag = True
+
+print("Ist heute ein Feiertag? (ja/nein)")
+feiertagInput = input()
+feiertagInput = feiertagInput.lower()
+if feiertagInput == "ja":
+    feiertag = True
+
+if sonntag:
+    print("Das Geschäft ist heute geschlossen. Einen schönen Sonntag!")
+elif feiertag:
+    print("Das Geschäft ist heute geschlossen.")
+else:
+    print("Das Geschäft ist heute geöffnet.")
