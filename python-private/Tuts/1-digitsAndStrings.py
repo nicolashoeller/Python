43edd623a589d96b9eec566820b91b0c1f029813
nicolashoeller <hoellr3.0@gmail.com>
2024-02-25 14:36:24 +0100

    Add code for string manipulation and basic arithmetic operations

---
+++ python-private/Tuts/1-digitsAndStrings.py
@@ -0,0 +1,17 @@
+def strings():
+    text = "Hello World, how'r you?"
+    print(text)
+
+def digits():
+    digit = 1 + 1
+    digit1 = digit * 2
+    digit2 = digit1 / 2
+
+    print(digit2)
+
+def main():
+    strings()
+    digits()
+
+if __name__ ==  "__main__":
+    main()
