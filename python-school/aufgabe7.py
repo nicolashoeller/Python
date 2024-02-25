4f53d6bc107550c43120e0f9f08b90692e72e074
nicolashoeller <hoellr3.0@gmail.com>
2024-02-25 14:36:19 +0100

    Add phone number to Philipp's contact information

---
+++ python-school/aufgabe7.py
@@ -0,0 +1,7 @@
+philipp = {'Vorname' : "Philipp", 'Nachname' : "Heidenberger", 'Alter' : 16}
+
+number = input("Geben Sie Ihre Telefonnummer ein: ")
+
+philipp['Telefonnummer'] = number
+
+print(philipp)
