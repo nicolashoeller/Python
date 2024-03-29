4c0d92d741f8408c1dc0d1c68c363b0148f44835
nicolashoeller <hoellr3.0@gmail.com>
2024-03-29 15:19:51 +0100

    Update file path in Extraction 2.py

--- python-private/Hacking/chronossec/Extraction 2/Extraction 2.py
+++ python-private/Hacking/chronossec/Extraction 2/Extraction 2.py
@@ -2,7 +2,7 @@ import re
 
 md5_pattern = re.compile(r'^[a-fA-F0-9]{32}$')
 
-with open('/home/kali/Desktop/Programming/Python/python-private/Hacking/chronossec/file.txt', 'r') as file:
+with open('/home/kali/Desktop/Programming/Python/python-private/Hacking/chronossec/Extraction 2/file.txt', 'r') as file:
     for line in file:
         line = line.strip()
         if not md5_pattern.match(line):
